3373f2c180f4be7c647a89f0a6aef091ae39229e
Carl Puerschner <>
2025-03-05 18:04:44 -0700

    fucked something up

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // Try without .tsx first (preferred in TS)
 import './index.css';
 
 const rootElement = document.getElementById('root');
